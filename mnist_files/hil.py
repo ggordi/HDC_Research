7b915e4586b53e31b0103f4d0ab1a2afe1ed4b5c
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-05-28 11:45:12 -0400

    hil file updates

--- mnist_files/hil.py
+++ mnist_files/hil.py
@@ -25,6 +25,8 @@ def print_hd(vector):
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
+# for now writing out the encoded vectors explicitly, will soon change to an iterative approach
+
 # class 0 expected for train images: 1, 2, 4, 10, 17, 26, 48
 s0 = vec.consensus_sum([encode(train_images[1]), encode(train_images[2]), encode(train_images[4])])
 b0 = vec.xor(s0, output_vecs[0])
@@ -42,18 +44,13 @@ s3 = vec.consensus_sum([encode(train_images[3]), encode(train_images[20]), encod
 b3 = vec.xor(s3, output_vecs[3])
 
 # class 4: 19, 22, 24, 28, 29, 68, 75, 76, 96
-
 # class 5: 8, 9, 12, 13, 30, 36, 43, 60, 62, 63, 82
-
 # class 6: 18, 32, 33, 39, 40, 55, 56, 72, 77, 95
-
 # class 7: 6, 14, 41, 46, 52, 83, 85, 87
-
 # class 8: 23, 35, 57, 99
-
 # class 9: 0, 11, 15, 42, 44, 79, 84, 88, 89, 90, 93
 
-t = encode(train_images[48])
+t = encode(train_images[48])  # expected output class of 0
 res = vec.xor(t, b0)
 print(vec.hamming_distance(res, output_vecs[0]))
 res = vec.xor(t, b1)
@@ -62,3 +59,11 @@ res = vec.xor(t, b2)
 print(vec.hamming_distance(res, output_vecs[0]))
 res = vec.xor(t, b3)
 print(vec.hamming_distance(res, output_vecs[0]))
+# above, we see the expected minimum hamming distance to only the corresponding output class of 0
+
+hil = vec.consensus_sum([b0, b1, b2, b3])
+res = vec.xor(t, hil)
+print_hd(res)
+# unexpected behavior: seeing a hamming distance of ~4k for any class included in the hil
+
+
