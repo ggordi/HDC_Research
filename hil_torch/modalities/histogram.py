5dde35487a4fc08752a6264ce920a00320914956
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-16 21:42:05 -0400

    Update histogram encoding method

--- hil_torch/modalities/histogram.py
+++ hil_torch/modalities/histogram.py
@@ -2,6 +2,7 @@ import vector as vec
 from bases import output_vecs
 from intensities import intensity_vecs
 import numpy as np
+import tensorflow as tf
 
 
 # create semantic count vectors, max count is total number of pixels in the image
@@ -19,16 +20,21 @@ def create_count_vecs(max_count):
 
     return vec_list
 
+
+max_count = 28 * 28  # images in the fashion mnist dataset are 28 by 28 pixels
 count_vecs = create_count_vecs(28 * 28)
 
 
-# create histogram dictionary for entered matrix of intensity values
+# create histogram for entered image (matrix of intensity values)
 def create_histogram(matrix, width, height):
     hist = {}
     for row in range(0, width):
         for col in range(0, height):
             pix = matrix[row][col]
-            hist[pix] = hist.get(pix, 0) + 1  # set to 1 if not present
+            if pix in hist:
+                hist[pix] += 1
+            else:
+                hist[pix] = 1
     return hist
 
 
@@ -41,6 +47,53 @@ def encode_histogram(matrix, width, height):
     return vec.consensus_sum(vec_list)
 
 
+# train and test the model
+def train_test(num_examples):
+    # train
+    class_vecs = {i: [] for i in range(10)}
+
+    for i in range(num_examples):
+        class_vecs[train_labels[i]].append(encode_histogram(train_images[i], 28, 28))
+
+    sums = []
+    for i in range(10):
+        cons = vec.consensus_sum(class_vecs[i])
+        if cons is not None:
+            sums.append(cons)
+
+    bound_vecs = []
+    count = 0
+    for sum_vec in sums:
+        bound_vecs.append(vec.xor(sum_vec, output_vecs[count]))
+        count += 1
+
+    hil = vec.consensus_sum(bound_vecs)
+
+    # test
+    correct = 0
+    for i in range(1000):
+        t = encode_histogram(test_images[i], 28, 28)
+        res = vec.xor(hil, t)
+
+        min_hd = 10000
+        prediction = -1
+        for i in range(10):
+            cur_hd = vec.hamming_distance(res, output_vecs[i])
+            if cur_hd < min_hd:
+                min_hd = cur_hd
+                prediction = i
+        expected = test_labels[i]
+
+        if prediction == expected:
+            correct += 1
+
+    return correct / 1000
+
+
 # train the model and test it
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
+print(train_test(50000))
 
+# achieved 13.6% accuracy when trained on 10,000 examples and 35.8% accuracy when trained on 50,000 examples
+# in both cases, 1000 images from the test set were used
