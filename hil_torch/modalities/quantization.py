4390cec7329553aebf804861d2d79bdf08706d05
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-22 11:55:33 -0400

    Update image encoding

--- hil_torch/modalities/quantization.py
+++ hil_torch/modalities/quantization.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import vector as vec
-from bases import output_vecs
+from bases import output_vecs, row_vecs, col_vecs
 
 
 # calculated mean pixel intensity over the first 10000 images = 73.009
@@ -48,47 +48,65 @@ def calculate_bins():
 edges = [0, 11, 42, 69, 93, 116, 136, 155, 171, 185, 197, 207, 217, 226, 237, 255]  # calculate_bins() num_bins = 30
 
 # encode bins so the central vector represents the mean, increase hamming distance to mean vector as move over bins
-# use something like (difference in pixels) * 20 count bit changes per bin
+# (difference in pixels) * 20 count bit changes per bin
 # so, going from bin 0 to bin 1, 0 to 11, will change 11 - 0 = 11 * 20 = 220 bits
-# --> 255 * 20 = 5100 total changes (orthogonality)
+# --> 255 * 20 = 5100 max total changes (orthogonality)
 # notes about changing digits:
 # - for preceding vectors, start from the beginning of the vector (flip bits starting at 0)
 # - for ensuing vectors, start flipping digits from the end of the vector (starting at 10000)
+def encode_bins():
+    bin_vecs = {3: vec.Vector()}  # represents the mean, bin 3 (mean intensity 73 maps to bin 4)
 
-bin_vecs = {3: vec.Vector()}  # represents the mean, bin 3 (mean intensity 73 maps to bin 4)
+    cur = 3
+    idx = 0
 
-cur = 3
-idx = 0
+    print(bin_vecs[cur])
 
-print(bin_vecs[cur])
+    for i in range(2, -1, -1):  # encode vectors for the bins preceding center
+        dif = (edges[cur] - edges[i]) * 20  # how many bits to flip
+        cur_vec = bin_vecs[cur]
+        next_bits = cur_vec.bits.clone()  # type = tensor
+        for j in range(idx, idx + dif + 1):
+            next_bits[j] = next_bits[j] ^ 1  # xor with 0 to flip digit
+        idx += dif
+        bin_vecs[cur - 1] = vec.Vector(next_bits)
+        cur -= 1
 
-for i in range(2, -1, -1):  # encode vectors for the bins preceding center
-    dif = (edges[cur] - edges[i]) * 20  # how many bits to flip
-    cur_vec = bin_vecs[cur]
-    next_bits = cur_vec.bits.clone()  # type = tensor
-    for j in range(idx, idx + dif + 1):
-        next_bits[j] = next_bits[j] ^ 1  # xor with 0 to flip digit
-    idx += dif
-    bin_vecs[cur-1] = vec.Vector(next_bits)
-    cur -= 1
+    cur = 3
+    idx = 9999
+    for i in range(4, 16):  # encode vectors for the bins following center
+        dif = (edges[i] - edges[cur]) * 20
+        cur_vec = bin_vecs[cur]
+        next_bits = cur_vec.bits.clone()
+        for j in range(idx, idx - dif - 1, -1):
+            next_bits[j] = next_bits[j] ^ 1
+        idx -= dif
+        bin_vecs[cur + 1] = vec.Vector(next_bits)
+        cur += 1
 
-cur = 3
-idx = 9999
-for i in range(4, 10):  # encode vectors for the bins following center
-    dif = (edges[i] - edges[cur]) * 20
-    cur_vec = bin_vecs[cur]
-    next_bits = cur_vec.bits.clone()
-    for j in range(idx, idx - dif - 1, -1):
-        next_bits[j] = next_bits[j] ^ 1
-    idx -= dif
-    bin_vecs[cur+1] = vec.Vector(next_bits)
-    cur += 1
-
-print(bin_vecs)
+    return bin_vecs
 
 
-for i in range(0, 9):
-    print(f'hd vec {i} to {i+1} = {vec.hamming_distance(bin_vecs[i], bin_vecs[i+1])}')
+bins = encode_bins()
+
+
+# encode an image (matrix of pixel intensities) - each image is the sum of its pixel intensities bound to their location
+def encode_image(matrix):
+    pixel_vecs = []
+
+    for row in range(0, len(matrix)):
+        for col in range(0, len(matrix[row])):
+            idx = np.digitize(matrix[row][col], edges) - 1
+            intensity_vec = bins[idx]
+            pixel_vecs.append(vec.xor(vec.xor(intensity_vec, row_vecs[row]), col_vecs[col]))  # bind intensity to loc
+
+    return vec.consensus_sum(pixel_vecs)
+
+img = [[10, 200, 89], [0, 199, 243], [109, 109, 255]]  # test encode_img 
+print(encode_image(img).bits[:100])
+
+
+
 
 
 
