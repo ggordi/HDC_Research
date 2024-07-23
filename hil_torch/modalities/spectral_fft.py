e6039379e6928ff5c5b9ce9db66c41c4f9d8159d
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-23 16:27:24 -0400

    Binning calculations

--- hil_torch/modalities/spectral_fft.py
+++ hil_torch/modalities/spectral_fft.py
@@ -6,15 +6,16 @@ import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
-img = train_images[0]
+for i in range(0, 4):
+    img = train_images[i * 46]
 
-img_fft = np.fft.fft2(img)  # compute 2d fft
-shifted_fft = np.fft.fftshift(img_fft)  # shift zero to center
-mag_spec = np.abs(shifted_fft)  # magnitude spectrum
+    img_fft = np.fft.fft2(img)  # compute 2d fft
+    shifted_fft = np.fft.fftshift(img_fft)  # shift zero to center
+    mag_spec = np.abs(shifted_fft)  # magnitude spectrum
 
-plt.title('magnitude spectrum')
-plt.imshow(mag_spec)
-plt.colorbar()
+    mag_spec = np.log1p(mag_spec)  # log scale for better visualization
+    plt.subplot(1, 4, i+1)
+    plt.imshow(mag_spec)
+    plt.colorbar()
+
 plt.show()
-
-
