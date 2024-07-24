939fb7cd94d41f2bff84783e4f9209bf2cd33c25
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-24 17:55:19 -0400

    FFT, compression, and start binning

--- hil_torch/modalities/spectral_fft.py
+++ hil_torch/modalities/spectral_fft.py
@@ -6,15 +6,34 @@ import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
-for i in range(0, 4):
-    img = train_images[i * 46]
-    img_fft = np.fft.fft2(img)  # compute 2d fft
-    shifted_fft = np.fft.fftshift(img_fft)  # shift zero to center
-    mag_spec = np.abs(shifted_fft)  # magnitude spectrum
 
-    mag_spec = np.log1p(mag_spec)  # log scale for better visualization
-    plt.subplot(1, 4, i+1)
-    plt.imshow(mag_spec)
-    plt.colorbar()
+# linearly encode bins for the magnitudes of the frequencies in the images
+def encode_bins(num_bins):
+    f_train = np.abs(np.fft.fft2(train_images))
+    min_magnitude = np.min(f_train)
+    max_magnitude = np.max(f_train)
+    bins = np.linspace(min_magnitude, max_magnitude, num_bins)
+    print(bins)
 
-plt.show()
+
+def highpass_filter(matrix, drop_pctg):
+    # fft the image and center zero frequency
+    f = np.fft.fft2(matrix)
+    f = np.fft.fftshift(f)
+
+    # compress the image
+    rows, cols = f.shape
+    drop_count = int(drop_pctg * min(rows, cols))  # calculate number of frequencies to drop
+    # drop frequencies nearest to the center, which are the lowest
+    f[int(rows / 2 - drop_count):int(rows / 2 + drop_count), int(cols / 2 - drop_count):int(cols / 2 + drop_count)] = 0
+
+    # bin the frequencies
+
+
+encode_bins(32)
+
+# next steps:
+# - going to encode based on the magnitudes of each frequency (that's the feature the model should be learning)
+# - use np.abs to get the magnitude
+# - then, bin each magnitude to a certain bin vector (encode these bins, can be linearly/equidistant, use
+#   np.digitize like in quantization file)
