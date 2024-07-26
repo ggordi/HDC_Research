74c0a1188faec38bc577f4cad9b15fb8a52fcb18
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-25 22:24:00 -0400

    Adding positional basis vectors

--- hil_torch/bases.py
+++ hil_torch/bases.py
@@ -3,3 +3,4 @@ from vector import Vector
 output_vecs = [Vector() for _ in range(10)]
 row_vecs = [Vector() for _ in range(28)]
 col_vecs = [Vector() for _ in range(28)]
+pos_vecs = [Vector() for _ in range(100)]
