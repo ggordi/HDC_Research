215392f4eb9658ef082b756c44c13759f006aecc
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-07-31 22:28:39 -0400

    Code reorganization, reformatting

--- hil_torch/modalities/convolutional.py
+++ hil_torch/modalities/convolutional.py
@@ -1,8 +1,6 @@
-# using hypervector encodings of convolutional feature signals from a CNN
-
-import vector as vec
-import numpy as np
-from bases import output_vecs, pos_vecs
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------- Training the CNN ------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
 
 import torch
 import torchvision
@@ -10,16 +8,6 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-transform = transforms.ToTensor()
-device = 'cuda'
-
-batch_size = 10
-train_set = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
-test_set = torchvision.datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
-classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot')
-
-train_set, validation_set = torch.utils.data.random_split(train_set, [50000, 10000])
 
 
 class ConvoNet(nn.Module):
@@ -64,191 +52,185 @@ class ConvoNet(nn.Module):
         x = self.drop1(x)
 
         x = self.out(x)
-        normalized_output = torch.tanh(x)
 
-        return x, normalized_output
-
-
-def encode_bins(num_bins):
-    edges = np.linspace(-1, 1, num_bins)
-    bin_vecs = [vec.Vector()]
-
-    num_changes = 5000 // num_bins
-
-    for i in range(num_bins):  # gradually change the vector for the first bin
-        new_bits = bin_vecs[-1].bits.clone()
-        for j in range(i * num_changes, (i + 1) * num_changes):
-            new_bits[j] ^= 1
-        bin_vecs.append(vec.Vector(new_bits))
-
-    return bin_vecs, edges
+        return x
 
 
-# encodes an image into vector form by using the output layer of the CNN
-def encode_nn(img):
-    net.eval()
-    with torch.no_grad():
-        _, normalized_output = net(img.unsqueeze(0))
-        digitized = np.digitize(normalized_output.cpu().numpy(), bins=edge_vals)[0]
+def train_epoch():
+    net.train(True)
+    total_loss = 0.0
+    correct = 0.0
+    total = 0
 
-        print(normalized_output)
-        print(digitized)
+    i = 0
 
-        vecs = []
+    for data in train_loader:
+        print(f'training data #{i + 1} out of {len(train_loader)}')
+        i += 1
 
-        for i in range(len(digitized)):  # bind each signal bin to component position
-            vecs.append(vec.xor(bins[digitized[i]], pos_vecs[i]))
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
 
-        return vec.consensus_sum(vecs)
+        outputs = net(inputs)  # shape: [1, 10]
+        correct += torch.sum(labels == torch.argmax(outputs, dim=1)).item()
+
+        loss = criterion(outputs, labels)
+        total_loss += loss.item()
+        total += labels.size(0)
+
+        loss.backward()
+        optimizer.step()
+
+    avg_loss = total_loss / len(train_loader)
+    avg_acc = (correct / total) * 100
+    print(f'training loss: {avg_loss}, accuracy: {avg_acc}')
+
+    print()
 
 
-def train_test_hd(num_examples, train_loader, test_loader):
-    # train
-    class_vecs = {i: [] for i in range(10)}
+def validate_epoch():
+    net.train(False)
+    total_loss = 0.0
+    correct = 0
+    total_samples = 0
 
-    for i, data in enumerate(train_loader):
-        print(f'encoding vector #{i + 1}')
-        if i == num_examples:
-            break
+    for data in validation_loader:
         inputs, labels = data[0].to(device), data[1].to(device)
 
-        for image, label in zip(inputs, labels):
-            class_vecs[label.item()].append(encode_nn(image))
+        with torch.no_grad():
+            outputs = net(inputs)
+            correct += torch.sum(labels == torch.argmax(outputs, dim=1)).item()
+            total_loss += criterion(outputs, labels).item()
+            total_samples += labels.size(0)
 
-    sums = []
-    for i in range(10):
-        cons = vec.consensus_sum(class_vecs[i])
-        if cons is not None:
-            sums.append(cons)
+    avg_loss = total_loss / len(validation_loader)
+    avg_acc = (correct / total_samples) * 100
+    print(f'validation loss: {avg_loss}, validation accuracy: {avg_acc}\n')
 
-    bound_vecs = []
-    count = 0
-    for sum_vec in sums:
-        bound_vecs.append(vec.xor(sum_vec, output_vecs[count]))
-        count += 1
 
+def train(num_epochs=1):
+    for epoch_i in range(num_epochs):
+        print(f'training epoch {epoch_i}')
+        train_epoch()
+        validate_epoch()
+    print('training complete')
+
+
+transform = transforms.ToTensor()
+device = 'cuda'
+
+batch_size = 10
+train_set = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_set = torchvision.datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot')
+
+train_set, validation_set = torch.utils.data.random_split(train_set, [50000, 10000])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+net = ConvoNet()
+net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
+
+train()
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------- Hypervector encoding of CNN output signals  --------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# process: pass an image into the network. receive real valued vector of output signals. normalize with tan-h. bin
+# each value to the corresponding hypervector. bind to position symbolic vector. bundle across signals to create vector
+# representation for the image.
+
+import vector as vec
+import numpy as np
+from bases import pos_vecs, output_vecs
+
+
+def encode_bins(num_bins=32):
+    bin_edges = np.linspace(-1, 1, num_bins + 1)
+    bin_vectors = [vec.Vector()]
+    indexes = np.arange(10000)  # using vectors of length 10000
+
+    for i in range(num_bins):
+        bin_bits = bin_vectors[-1].bits.clone()
+        to_change = np.random.choice(indexes, size=10000 // num_bins, replace=False)
+        for idx in to_change:
+            bin_bits[idx] ^= 1
+        indexes = np.setdiff1d(indexes, to_change)
+        bin_vectors.append(vec.Vector(bin_bits))
+
+    return bin_vectors, bin_edges
+
+
+def encode_image(image):
+    signal_vectors = []
+    net.eval()
+    with torch.no_grad():
+        cnn_output = net(image.unsqueeze(0))  # the CNN expects a batch, give the image a batch index
+        binned = np.digitize(torch.tanh(cnn_output).cpu().numpy(), bins=edges)[0]
+        for i in range(len(binned)):  # bind the signal hypervector to the component position hypervector
+            signal_vectors.append(vec.xor(bins[binned[i] - 1], pos_vecs[i]))  # need -1 to adjust from digitize
+    return vec.consensus_sum(signal_vectors)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# --------------------------------------------- Training and Testing the HIL   --------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+def train_test(num_examples=1000):
+    net.eval()
+    # train
+    class_vecs = {i: [] for i in range(10)}
+    example_count = 0
+    with torch.no_grad():
+        for i, data in enumerate(train_loader):
+            if example_count == num_examples:
+                break
+            inputs, labels = data[0].to(device), data[1].to(device)
+            for image, label in zip(inputs, labels):
+                class_vecs[label.item()].append(encode_image(image))
+                example_count += 1
+
+    sum_vecs = [vec.consensus_sum(class_vecs[i]) for i in range(10)]
+    bound_vecs = [vec.xor(sum_vecs[i], output_vecs[i]) for i in range(10)]
     hil = vec.consensus_sum(bound_vecs)
 
     # test
     correct = 0
-    total = 0.0
-    for i, data in enumerate(test_loader):
-        if i == 100:  # testing on 100 batches
-            break
-        inputs, labels = data[0].to(device), data[1].to(device)
+    total = 0
 
-        for image, label in zip(inputs, labels):
-            print(f'testing vector #{i + 1}')
-            total += 1
-            t = encode_nn(image)
-            res = vec.xor(hil, t)
-            min_hd = 10000
-            prediction = -1
-            for j in range(10):
-                cur_hd = vec.hamming_distance(res, output_vecs[j])
-                if cur_hd < min_hd:
-                    min_hd = cur_hd
-                    prediction = j
-            if prediction == label.item():
-                correct += 1
-
-    return correct / total
-
-
-if __name__ == '__main__':
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
-    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=False, num_workers=2)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
-
-    net = ConvoNet()
-    net.to(device)
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0001)
-
-
-    def train_epoch():
-        net.train(True)
-        running_loss = 0.0
-        running_accuracy = 0.0
-        total_samples = 0
-
-        i = 0
-
-        for data in train_loader:
-            print(f'training data #{i + 1} out of {len(train_loader)}')
-            i += 1
-            inputs, labels = data[0].to(device), data[1].to(device)
-            optimizer.zero_grad()
-
-            outputs, _ = net(inputs)  # shape: [1, 10]
-            correct = torch.sum(labels == torch.argmax(outputs, dim=1)).item()
-            running_accuracy += correct
-
-            loss = criterion(outputs, labels)
-            running_loss += loss.item()
-            total_samples += labels.size(0)
-
-            loss.backward()
-            optimizer.step()
-
-        avg_loss = running_loss / len(train_loader)
-        avg_acc = (running_accuracy / total_samples) * 100
-        print(f'training loss: {avg_loss}, accuracy: {avg_acc}')
-
-        print()
-
-
-    def validate_epoch():
-        net.train(False)
-        running_loss = 0.0
-        correct = 0
-        total_samples = 0
-
-        for data in validation_loader:
+    with torch.no_grad():
+        for data in test_loader:
             inputs, labels = data[0].to(device), data[1].to(device)
 
-            with torch.no_grad():
-                outputs, _ = net(inputs)
-                correct += torch.sum(labels == torch.argmax(outputs, dim=1)).item()
-                loss = criterion(outputs, labels)
-                running_loss += loss.item()
-                total_samples += labels.size(0)
+            for image, label in zip(inputs, labels):
+                if total == 1000:  # test on 1000 examples
+                    break
+                img_vec = encode_image(image)
+                result = vec.xor(img_vec, hil)
+                min_hd = 10000
+                prediction = -1
+                for j in range(10):
+                    cur_hd = vec.hamming_distance(result, output_vecs[j])
+                    if cur_hd < min_hd:
+                        min_hd = cur_hd
+                        prediction = j
+                if prediction == label.item():
+                    correct += 1
+                total += 1
 
-        avg_loss = running_loss / len(validation_loader)
-        avg_acc = (correct / total_samples) * 100
-        print(f'validation loss: {avg_loss}, validation accuracy: {avg_acc}\n')
-
-
-    def train(num_epochs=1):
-        for epoch_i in range(num_epochs):
-            print(f'training epoch {epoch_i}')
-            train_epoch()
-            validate_epoch()
-        print('training complete')
-
-
-    def test():
-        net.eval()
-        correct = 0
-        total_samples = 0
-
-        with torch.no_grad():
-            for data in test_loader:
-                inputs, labels = data[0].to(device), data[1].to(device)
-                outputs, _ = net(inputs)
-                correct += torch.sum(labels == torch.argmax(outputs, dim=1)).item()
-                total_samples += labels.size(0)
-
-        print(f'test accuracy: {correct / total_samples * 100}')
+    return correct / 1000.0
 
 
-    train()
-    # test()
+bins, edges = encode_bins()
 
-    bins, edge_vals = encode_bins(50)
+print(train_test(50))
 
-    print(train_test_hd(1000, train_loader, test_loader))
+# 50,000 training examples, 71% accuracy
+# 50 training examples, 63% accuracy
 
-# results (tested on 100 batches of size 10):
-# 60.8% accuracy, trained w/ 1000 examples
+# --> need to run this script and visualize impact of training examples over time
