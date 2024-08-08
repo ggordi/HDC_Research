7a4ea5b67b0695aec8934dabf26f829f2656018e
Gili Gordiyenko <60244395+ggordi@users.noreply.github.com>
2024-08-07 20:11:21 -0400

    Major fix to encoding function

--- hil_torch/modalities/spectral_dct.py
+++ hil_torch/modalities/spectral_dct.py
@@ -1,58 +1,53 @@
-# note: need to update 3x3 blocking to include overlapping, as of now it does not.
+# encoding images using sparse spectral features, computed via discrete cosine transform
 
-# encoding images using sparse spectral features, using discrete cosine transform
-import numpy as np
 import tensorflow as tf
-from scipy.fftpack import dctn, idctn
+from scipy.fft import dctn
+import numpy as np
+
 import vector as vec
-import matplotlib.pyplot as plt
 from bases import output_vecs, pos_vecs
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
+def encode_bins():
+    bin_vectors = [vec.Vector()]
+    indexes = np.arange(10000)  # using vectors of length 10000
 
-def calculate_edges(num_bins):
-    d_train = []
-    for i in range(0, 26, 3):
-        for j in range(0, 26, 3):
-            block = train_images[:, i:i + 3, j:j + 3]
-            d_train.extend(np.abs(dctn(block)).flatten())
-    return np.percentile(np.sort(d_train), np.linspace(0, 100, num_bins + 1))
+    for i in range(num_bins):
+        bin_bits = bin_vectors[-1].bits.clone()
+        to_change = np.random.choice(indexes, size=10000 // num_bins, replace=False)
+        for idx in to_change:
+            bin_bits[idx] ^= 1
+        indexes = np.setdiff1d(indexes, to_change)
+        bin_vectors.append(vec.Vector(bin_bits))
 
-
-# adjust so hamming distance is proportionate to difference between bin frequency values
-def encode_bins(num_bins):
-    bins = [vec.Vector()]
-    changes = 5000 // num_bins  # 5000 represents randomness when vectors are of length 10000
-    for i in range(0, num_bins):
-        cur_vec = bins[i]
-        next_bits = cur_vec.bits.clone()
-        for j in range(changes * i, changes * (i + 1)):
-            next_bits[j] ^= 1
-            bins.append(vec.Vector(next_bits))
-    return bins
+    return bin_vectors
 
 
-def encode_dct(matrix):
-    # dct the image in 3x3 pixel blocks
+def get_edges(start, stop):
+    return list(np.linspace(start, stop, num_bins + 1))
+
+
+def encode_dct(image):
+    # process the image in 3x3 blocks
     blocks = []
     for i in range(0, 26, 3):
         for j in range(0, 26, 3):
-            blocks.append(matrix[i:i + 3, j:j + 3])
-    dct_blocks = [dctn(block) for block in blocks]
+            blocks.append(image[i:i + 3, j:j + 3])
 
-    # preserve only highest single frequency out of the 9 in each block, and bin them
-    freq = []
-    for block in dct_blocks:
-        freq.append(np.abs(np.max(block)))
-    block_bins = np.digitize(freq, edges)
+    # dct each block and retain the highest frequency, bin the values
+    highest_fre = []
+    for block in blocks:
+        dct_res = dctn(block, norm='ortho')
+        highest_fre.append(max(dct_res.flatten()))
+    fre_bins = np.digitize(highest_fre, edges)
 
-    # the index of the frequency in the list indicates its position in the image
-    freq_vecs = []
-    for i in range(0, len(block_bins)):
-        freq_vecs.append(vec.xor(bin_vecs[block_bins[i]], pos_vecs[i]))  # bind the bin to its location
+    # bind the bin vectors to their position
+    fre_vecs = []
+    for i, val in enumerate(fre_bins):
+        fre_vecs.append(vec.xor(pos_vecs[i], bin_vecs[val]))
 
-    return vec.consensus_sum(freq_vecs)
+    return vec.consensus_sum(fre_vecs)
 
 
 # train and test
@@ -100,9 +95,8 @@ def train_test(num_examples):
     return correct / 1000
 
 
-edges = calculate_edges(150)
-bin_vecs = encode_bins(150)
+num_bins = 32
+edges = get_edges(0, 800)  # max frequency values will range from 0 to less than 800
+bin_vecs = encode_bins()
 
-# print(train_test(1000))
-
-# 10.7% accuracy with 150 bins, 1000 examples
+print(train_test(10000))
